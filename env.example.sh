518071df432e130ec9fbd2844b2026ef6697538e
Kevin van Zonneveld <kevin@vanzonneveld.net>
2020-05-07 10:53:44 +0200

    Add secrets /cc @etienne-carriere

---
+++ env.example.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+# source env.sh
+# travis encrypt --add env.global "TRANSLOADIT_KEY=${TRANSLOADIT_KEY}"
+# travis encrypt --add env.global "TRANSLOADIT_SECRET=${TRANSLOADIT_SECRET}"
+export TRANSLOADIT_KEY=xxxxxxxx
+export TRANSLOADIT_SECRET=xxxxxxxxyyyy
