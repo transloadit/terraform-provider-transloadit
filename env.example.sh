6a1ea7b79793664d906862251bae5a447fde91c0
Kevin van Zonneveld <kevin@vanzonneveld.net>
2020-05-12 15:55:16 +0200

    Add GH_TOKEN

--- env.example.sh
+++ env.example.sh
@@ -2,5 +2,7 @@
 # source env.sh
 # travis encrypt --add env.global "TRANSLOADIT_KEY=${TRANSLOADIT_KEY}"
 # travis encrypt --add env.global "TRANSLOADIT_SECRET=${TRANSLOADIT_SECRET}"
+# travis encrypt --add env.global "GH_TOKEN=${GH_TOKEN}"
+export GH_TOKEN=xxxxxxxxyyyy
 export TRANSLOADIT_KEY=xxxxxxxx
 export TRANSLOADIT_SECRET=xxxxxxxxyyyy
