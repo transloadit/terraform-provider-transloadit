1ae9103869bfab6e4bc8832f02faa7e9c1fb56b8
Kevin van Zonneveld <kevin@vanzonneveld.net>
2020-05-14 08:46:14 +0200

    Update env.example.sh

--- env.example.sh
+++ env.example.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 # source env.sh
-# travis encrypt --add env.global "TRANSLOADIT_KEY=${TRANSLOADIT_KEY}"
-# travis encrypt --add env.global "TRANSLOADIT_SECRET=${TRANSLOADIT_SECRET}"
+# travis encrypt --add env.global "TRANSLOADIT_AUTH_KEY=${TRANSLOADIT_AUTH_KEY}"
+# travis encrypt --add env.global "TRANSLOADIT_AUTH_SECRET=${TRANSLOADIT_AUTH_SECRET}"
 # travis encrypt --add env.global "GH_TOKEN=${GH_TOKEN}"
 export GH_TOKEN=xxxxxxxxyyyy
-export TRANSLOADIT_KEY=xxxxxxxx
-export TRANSLOADIT_SECRET=xxxxxxxxyyyy
+export TRANSLOADIT_AUTH_KEY=xxxxxxxx
+export TRANSLOADIT_AUTH_SECRET=xxxxxxxxyyyy
