6d69f50e3a9dd3f51f123abfa99ed1bdf0b6bb7d
Kevin van Zonneveld <kevin@vanzonneveld.net>
2020-07-31 11:05:13 +0200

    Add SIGN_PASS /cc @etienne-carriere

--- env.example.sh
+++ env.example.sh
@@ -3,6 +3,8 @@
 # travis encrypt --add env.global "TRANSLOADIT_AUTH_KEY=${TRANSLOADIT_AUTH_KEY}"
 # travis encrypt --add env.global "TRANSLOADIT_AUTH_SECRET=${TRANSLOADIT_AUTH_SECRET}"
 # travis encrypt --add env.global "GH_TOKEN=${GH_TOKEN}"
+# travis encrypt --add env.global "SIGN_PASS=${SIGN_PASS}"
+export SIGN_PASS=xxx
 export GH_TOKEN=xxxxxxxxyyyy
 export TRANSLOADIT_AUTH_KEY=xxxxxxxx
 export TRANSLOADIT_AUTH_SECRET=xxxxxxxxyyyy
