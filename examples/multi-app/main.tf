35d78d50c64688c1b959989b87abed5081b99a78
Kevin van Zonneveld <kevin@vanzonneveld.net>
2021-08-03 14:56:28 +0200

    Do not use modules, as you can pass providers to resources just as well. Thank you @Etienne-Carriere

--- examples/multi-app/main.tf
+++ examples/multi-app/main.tf
@@ -8,33 +8,67 @@ terraform {
   }
 }
 
-variable "tl_urltransform_key" { type = string }
+// Define our `urltransform` App and its credentials
+variable "tl_urltransform_key"    { type = string }
 variable "tl_urltransform_secret" { type = string }
-variable "tl_preprocess_key" { type = string }
-variable "tl_preprocess_secret" { type = string }
-
 provider "transloadit" {
   auth_key    = var.tl_urltransform_key
   auth_secret = var.tl_urltransform_secret
   alias       = "urltransform"
 }
+
+// Define our `preprocess` App and its credentials
+variable "tl_preprocess_key"    { type = string }
+variable "tl_preprocess_secret" { type = string }
 provider "transloadit" {
   auth_key    = var.tl_preprocess_key
   auth_secret = var.tl_preprocess_secret
   alias       = "preprocess"
 }
 
-module "urltransform" {
-  source    = "./urltransform"
-  providers = {
-    transloadit = transloadit.urltransform
+resource "transloadit_template" "resize-img" {
+  provider = transloadit.urltransform // <-- In which App to save the template
+	name     = "resize-img"
+	template = <<EOT
+{
+  "steps": {
+    "imported": {
+      "robot": "/s3/import",
+      "credentials": "my_s3_$${assembly.region}",
+      "path": "/onthefly/$${fields.input}"
+    },
+    "resized": {
+      "use": "imported",
+      "robot": "/image/resize",
+      "width": "$${fields.w}",
+      "imagemagick_stack": "v2.0.7"
+    },
+    "served": {
+      "use": "resized",
+      "robot": "/file/serve"
+    }
   }
 }
-module "preprocess" {
-  source    = "./preprocess"
-  providers = {
-    transloadit = transloadit.preprocess
-  }
+EOT
 }
 
-output "cdn-resize-id" { value = module.urltransform.resize-img-id }
+resource "transloadit_template" "video-encode" {
+  provider = transloadit.preprocess // <-- In which App to save the template
+	name     = "video-encode"
+	template = <<EOT
+{
+  "steps": {
+    ":original": {
+      "robot": "/upload/handle"
+    },
+    "encoded": {
+      "use": ":original",
+      "robot": "/video/encode",
+      "preset": "ipad-high"
+    }
+  }
+}
+EOT
+}
+
+output "cdn-resize-id" { value = transloadit_template.resize-img.id }
